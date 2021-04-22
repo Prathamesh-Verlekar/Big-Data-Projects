c907f97ebbbc4c28f9ce27ecd72810640b0471a4
Prathamesh Shrikrishna Verlekar <verlekar.p@husky.neu.edu>
2021-04-22 01:25:21 -0400

    Adding new styling

---
+++ MongoDB/CRUD Node JS Express MongoDB/crud_app/server/model/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender : String,
+    status : String
+})
+
+const Employeedb = mongoose.model('employeedb', schema);
+
+module.exports = Employeedb;
